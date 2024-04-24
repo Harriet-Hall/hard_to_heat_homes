17646b60d3943d6e9156244f80b1d97187edbde4
Harriet <harriet.hall@madetech.com>
2024-04-24 12:57:35 +0100

    test: GREEN epc_call() handles invalid uprn

--- tests/test_epc_api.py
+++ tests/test_epc_api.py
@@ -15,24 +15,29 @@ HEADERS = {
     }
 
 def epc_call(headers, params):
-    
-    
-  
 
     encoded_params = urlencode(params)
     full_url = f'{BASE_URL}{encoded_params}'
+
     try:
         with urllib.request.urlopen(urllib.request.Request(full_url, headers=headers)) as response:
-            return response.status == 200
+            response_body = response.read()
+
+            if len(response_body.decode()) > 0:
+                return response.status == 200
+
+            return {}
     except Exception:
         return False
             
-        
-
-   
 
 def test_200_response():
     assert epc_call(HEADERS, QUERY_PARAMS) == True
 
+def test_200_response_invalid_uprn():
+    assert epc_call(HEADERS, {'uprn' : '123456789'}) == {}
+
+
 def test_not_200_response():
+    
     assert epc_call({}, QUERY_PARAMS) == False
