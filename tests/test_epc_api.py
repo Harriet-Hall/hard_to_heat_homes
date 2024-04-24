c17110cb78a2fd6112dfba985ad95451f480aaa5
Harriet <harriet.hall@madetech.com>
2024-04-24 12:37:25 +0100

    test: GREEN, epc_call() returns false when unauthorised

--- tests/test_epc_api.py
+++ tests/test_epc_api.py
@@ -5,28 +5,34 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+TOKEN = os.getenv('EPC_ENCODED_API_TOKEN')
+
 QUERY_PARAMS = {'uprn':'200002791'}
 BASE_URL = 'https://epc.opendatacommunities.org/api/v1/domestic/search?'
-
-def epc_call():
-    TOKEN = os.getenv('EPC_ENCODED_API_TOKEN')
-    
-    headers = {
+HEADERS = {
         'Accept': 'application/json',
         'Authorization': f'Basic {TOKEN}'
     }
 
-    encoded_params = urlencode(QUERY_PARAMS)
-    full_url = f'{BASE_URL}{encoded_params}'
+def epc_call(headers, params):
+    
+    
+  
 
-    with urllib.request.urlopen(urllib.request.Request(full_url, headers=headers)) as response:
-        return response.status == 200
+    encoded_params = urlencode(params)
+    full_url = f'{BASE_URL}{encoded_params}'
+    try:
+        with urllib.request.urlopen(urllib.request.Request(full_url, headers=headers)) as response:
+            return response.status == 200
+    except Exception:
+        return False
+            
         
 
-    return True
+   
 
 def test_200_response():
-    assert epc_call() == True
+    assert epc_call(HEADERS, QUERY_PARAMS) == True
 
-# def test_not_200_response():
-#     assert epc_call() == False
+def test_not_200_response():
+    assert epc_call({}, QUERY_PARAMS) == False
