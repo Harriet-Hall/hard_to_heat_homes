b6efd960073ea7aec068f448c1f6e7617c2e24b5
“Abid” <abdur.abid@madetech.com>
2024-04-25 14:31:13 +0100

    test:GREEN property class has epc_rating attribute

--- tests/test_property_class.py
+++ tests/test_property_class.py
@@ -12,20 +12,29 @@ HEADERS = {
         'Authorization': f'Basic {TOKEN}'
     }
 
+epc_test_property = epc_api_call(HEADERS, QUERY_PARAMS)['rows'][0]
+
 class Property():
-    def __init__(self, uprn):
+    def __init__(self, uprn, epc_rating):
         self.uprn = uprn
+        self.epc_rating = epc_rating
 
 def test_property_has_uprn():
-    dummy_property = Property('200002791')
+    dummy_property = Property('200002791', None)
     assert dummy_property.uprn == '200002791'
 
 def test_property_has_uprn_from_dummy_data():
-    dummy_property = Property(dummy_data['rows'][0]['uprn'])
+    dummy_property = Property(dummy_data['rows'][0]['uprn'], None)
     assert dummy_property.uprn == '200002791'
 
 def test_property_has_uprn_from_api_call():
-    dummy_property = Property(epc_api_call(HEADERS, QUERY_PARAMS)['rows'][0]['uprn'])
+    dummy_property = Property(epc_test_property['uprn'], None)
     assert dummy_property.uprn == '200002791'
+
+def test_property_has_EPC_rating():
+    dummy_property = Property('200002791', 'D')
+    assert dummy_property.epc_rating == 'D'
+    
+
 
 
