1def58c4840c74ae5664f2f19d96a334f80d7c3e
“Abid” <abdur.abid@madetech.com>
2024-04-25 16:44:23 +0100

    test: GREEN get_props() ==> list of Property instances

--- tests/test_multiple_properties.py
+++ tests/test_multiple_properties.py
@@ -1,4 +1,5 @@
 from app.main.epc_api import epc_api_call
+from app.main.property import Property
 from dotenv import load_dotenv
 import os 
 
@@ -12,7 +13,22 @@ HEADERS = {
         'Authorization': f'Basic {TOKEN}'
     }
 
+def get_props(props):
+    result = []
+    for prop in props:
+        result.append(Property(prop['uprn'], prop['current-energy-rating'], prop['current-energy-efficiency'], prop['address'], prop['postcode']))
 
-def test_returns_two_properties():
+    # print(f'>>>>>>>{result[0].address}')
+    return result
+
+
+def test_returns_two_rows_of_properties():
     array_of_properties = epc_api_call(HEADERS, {'local-authority' :'E09000008', 'size' : '2'})['rows']
     assert len(array_of_properties) == 2
+
+def test_returns_an_array_of_two_property_instances():
+    array_of_properties = epc_api_call(HEADERS, {'local-authority' :'E09000008', 'size' : '2'})['rows']
+    props = get_props(array_of_properties)
+    assert type(props) is list
+    assert type(props[0]) is Property
+    
