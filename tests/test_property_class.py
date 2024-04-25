749cce936c5da6e4dd83d425ae9b3f1aad5eaef3
“Abid” <abdur.abid@madetech.com>
2024-04-25 14:18:10 +0100

    test:GREEN property class has uprn attribute from dummy_data

--- tests/test_property_class.py
+++ tests/test_property_class.py
@@ -8,5 +8,7 @@ def test_property_has_uprn():
     dummy_property = Property('200002791')
     assert dummy_property.uprn == '200002791'
 
-
+def test_property_has_uprn_from_dummy_data():
+    dummy_property = Property(dummy_data['rows'][0]['uprn'])
+    assert dummy_property.uprn == '200002791'
 
